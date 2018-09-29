51ebe27f468760aa080a62fc25b015aadc383217
Ryan Marcotte <RyanM@iqmetrix.com>
2018-09-29 13:38:12 -0600

    page content

--- FinancialFoundations.SubjectMatter.Domain/SubjectMatterUnitPage.cs
+++ FinancialFoundations.SubjectMatter.Domain/SubjectMatterUnitPage.cs
@@ -4,13 +4,15 @@ namespace FinancialFoundations.SubjectMatter.Domain
 {
 	public class SubjectMatterUnitPage
 	{
-		public SubjectMatterUnitPage(Guid educatorID, Guid subjectMatterUnitID)
+		public SubjectMatterUnitPage(Guid educatorID, Guid subjectMatterUnitID, string content)
 		{
 			EducatorID = educatorID;
 			SubjectMatterUnitID = subjectMatterUnitID;
+			Content = content ?? throw new ArgumentNullException(nameof(content));
 		}
 
 		public Guid EducatorID { get; }
 		public Guid SubjectMatterUnitID { get; }
+		public string Content { get; }
 	}
 }
