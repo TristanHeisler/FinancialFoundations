44bccf63ee4c14a529985c7531b5bd7b941fae94
Ryan Marcotte <RyanM@iqmetrix.com>
2018-09-28 21:19:54 -0600

    register CQS handlers in implementation assemblies with SimpleInjector

---
+++ FinancialFoundations.SubjectMatter.Domain/_AssemblyMarker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FinancialFoundations.SubjectMatter.Domain
+{
+	// ReSharper disable once UnusedMember.Global
+	public class AssemblyMarker
+	{
+	}
+}
---
+++ FinancialFoundations.SubjectMatter.Implementation.LocalFileStorage/_AssemblyMarker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FinancialFoundations.SubjectMatter.Implementation.LocalFileStorage
+{
+	// ReSharper disable once UnusedMember.Global
+	public class AssemblyMarker
+	{
+	}
+}
