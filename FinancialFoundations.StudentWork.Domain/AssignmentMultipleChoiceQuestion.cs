0a73d47a72f218cb07d69adc1078f3d1d342efea
Tristan Heisler <tristanheisler@hotmail.ca>
2018-09-29 18:09:05 -0600

    Progress on answers.

--- FinancialFoundations.StudentWork.Domain/AssignmentMultipleChoiceQuestion.cs
+++ FinancialFoundations.StudentWork.Domain/AssignmentMultipleChoiceQuestion.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 
 namespace FinancialFoundations.StudentWork.Domain
 {
-	public class AssignmentMultipleChoiceQuestion
-	{
-		public AssignmentMultipleChoiceQuestion(Guid questionID, string questionText, IEnumerable<AssignmentMultipleChoiceQuestionAnswer> answerCollection)
-		{
-			QuestionID = questionID;
-			QuestionText = questionText ?? throw new ArgumentNullException(nameof(questionText));
-			AnswerCollection = answerCollection ?? throw new ArgumentNullException(nameof(answerCollection));
-		}
+    public class AssignmentMultipleChoiceQuestion
+    {
+        public AssignmentMultipleChoiceQuestion(Guid questionID, string questionText, IEnumerable<AssignmentMultipleChoiceQuestionAnswer> answerCollection)
+        {
+            QuestionID = questionID;
+            QuestionText = questionText ?? throw new ArgumentNullException(nameof(questionText));
+            AnswerCollection = answerCollection ?? throw new ArgumentNullException(nameof(answerCollection));
+        }
 
-		public Guid QuestionID { get; }
-		public string QuestionText { get; }
-		public IEnumerable<AssignmentMultipleChoiceQuestionAnswer> AnswerCollection { get; }
+        public Guid QuestionID { get; }
+        public string QuestionText { get; }
+        public IEnumerable<AssignmentMultipleChoiceQuestionAnswer> AnswerCollection { get; }
 	}
 }
